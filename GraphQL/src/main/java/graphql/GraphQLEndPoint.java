dbb179f5ac52047579a5c8e87162935a61b24a26
Leith <saddouri.leith@esprit.tn>
2026-04-21 14:05:31 +0100

    atelier ready

--- GraphQL/src/main/java/graphql/GraphQLEndPoint.java
+++ GraphQL/src/main/java/graphql/GraphQLEndPoint.java
@@ -1,7 +1,6 @@
-import com.coxautodev.graphql.tools.GraphQLResolver;
+package graphql;
+
 import com.coxautodev.graphql.tools.SchemaParser;
-import graphql.MutationResolver;
-import graphql.QueryResolver;
 import graphql.schema.GraphQLSchema;
 import graphql.servlet.SimpleGraphQLServlet;
 
@@ -16,11 +15,9 @@ public class GraphQLEndPoint extends SimpleGraphQLServlet {
 
     private static GraphQLSchema buildSchema() {
         return SchemaParser.newParser()
-               .file("schema.graphql")
-               .resolvers(new QueryResolver(), new MutationResolver())
-               .build()
-               .makeExecutableSchema();
-
+                .file("schema.graphql")
+                .resolvers(new QueryResolver(), new MutationResolver())
+                .build()
+                .makeExecutableSchema();
     }
-
 }
